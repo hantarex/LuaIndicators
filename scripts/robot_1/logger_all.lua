b39c85bc0252664eb2a0b2c7075a75b681637e46
ashikov <6546@mail.ru>
2019-03-11 18:36:43 +0300

    All data logger v.1.1

--- scripts/robot_1/logger_all.lua
+++ scripts/robot_1/logger_all.lua
@@ -24,6 +24,21 @@ function OnStop()
     is_run = false
 end
 
+AllTraiding = {}
+function AllTraiding:calc()
+    local end_index = getNumberOf("all_trades")
+    SearchItems ("all_trades", self.start_index, end_index, bind(self,'dataTraiding'))
+    self.start_index = end_index
+end
+
+function AllTraiding:dataTraiding(traid)
+    for key,val in pairs(self.config.SEC_CODES) do
+        if traid.sec_code == val then
+            PrintDbgStr(inspect(traid.qty))
+        end
+    end
+end
+
 DataSourceClass = {}
 DataSourceClass.__index = DataSourceClass
 setmetatable(DataSourceClass, {
@@ -36,8 +51,11 @@ function DataSourceClass:create(options)
     local init = {
         ds = options.ds,
         SEC_CODES = options.SEC_CODES,
-        end_candle = options.ds:Size()
+        CLASS_CODE = options.CLASS_CODE,
+        end_candle = options.ds:Size(),
     }
+--    PrintDbgStr(inspect(ParamRequest(init.CLASS_CODE, init.SEC_CODES,"qty")))
+--    PrintDbgStr(inspect(getParamEx2(init.CLASS_CODE, init.SEC_CODES,"qty")))
     return setmetatable(init, { __index = DataSourceClass })
 end
 
@@ -45,7 +63,8 @@ function DataSourceClass:data(index)
     if self.end_candle > index then
         return
     end
-    PrintDbgStr(inspect(index))
+    AllTraiding:calc()
+--    PrintDbgStr(inspect(index))
     self.end_candle = self.ds:Size()
 end
 
@@ -61,24 +80,36 @@ setmetatable(LoggerClass, {
 
 function LoggerClass:create(options)
     local init = {
-        config = jsonStorage.loadTable(getScriptPath().."/config/"..config)
+        config = jsonStorage.loadTable(getScriptPath().."/config/"..config),
     }
+    init.start_index = getNumberOf("all_trades")-1;
     return setmetatable(init, { __index = LoggerClass })
 end
 
 function LoggerClass:initData()
     local instanse = {}
+    AllTraiding.start_index = getNumberOf("all_trades")-1;
+    AllTraiding.config = self.config
     for key,val in pairs(self.config.SEC_CODES) do
+        PrintDbgStr(inspect(Subscribe_Level_II_Quotes(self.config.CLASS_CODE, val)))
         local ds = CreateDataSource(self.config.CLASS_CODE, val, INTERVAL_M5)
         local data = DataSourceClass({
             ds = ds,
-            SEC_CODES = val
+            SEC_CODES = val,
+            CLASS_CODE = self.config.CLASS_CODE
         })
         ds:SetUpdateCallback(bind(data, 'data'))
         table.insert(instanse,data)
     end
     self.instanse = instanse
 end
+
+--function LoggerClass:startTraidingMonitoring()
+--    local end_index = getNumberOf("all_trades")-1
+--    PrintDbgStr(inspect(end_index))
+--    SearchItems ("all_trades", self.start_index, end_index, bind(self,'dataTraiding'))
+--    self.start_index = end_index
+--end
 
 function bind(cls, method)
     return function(...) cls[method](cls,...) end
